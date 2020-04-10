c2a95bbebbe2d8d315d003e35efb57960594a71d
Alexander <terechovalexanderr@gmail.com>
2020-04-10 18:47:23 +0300

    Add infinity scroll

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/core/styles';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const useStyles = makeStyles({
   closeButton: {
@@ -22,11 +23,16 @@ function App() {
   const [row, setRow] = useState(null);
   const [page, setPage] = useState(0);
   const [hits, setHits] = useState([]);
+  const [more, setMore] = useState(false);
 
   let timer = useRef(false);
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
   useEffect(() => {
@@ -37,6 +43,13 @@ function App() {
         )
         .then((response) => {
           setHits((prevHits) => [...prevHits, ...response.data.hits]);
+
+          if (page > response.data.nbPages - 1) {
+            setMore(false);
+            clearInterval(timer.current);
+          } else {
+            setMore(true);
+          }
         })
         .catch((err) => console.log(err));
     };
@@ -47,7 +60,7 @@ function App() {
   useEffect(() => {
     timer.current = setInterval(() => {
       setPage((prevPage) => prevPage + 1);
-    }, 10000);
+    }, 1000);
 
     return () => {
       clearInterval(timer.current);
@@ -56,23 +69,37 @@ function App() {
 
   return (
     <div>
-      <MaterialTable
-        columns={[
-          { title: 'Title', field: 'title' },
-          { title: 'URL', field: 'url', filtering: false },
-          { title: 'Created', field: 'created_at', searchable: false },
-          { title: 'Autor', field: 'author', filtering: false },
-        ]}
-        data={hits}
-        options={{
-          paging: false,
-          filtering: true,
-        }}
-        onRowClick={(event, row) => {
-          setRow(row);
-          setOpen(true);
-        }}
-      />
+      <InfiniteScroll
+        dataLength={hits.length}
+        next={loadMore}
+        hasMore={more}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <MaterialTable
+          columns={[
+            { title: 'Title', field: 'title' },
+            { title: 'URL', field: 'url', filtering: false },
+            { title: 'Created', field: 'created_at', searchable: false },
+            { title: 'Autor', field: 'author', filtering: false },
+          ]}
+          data={hits}
+          options={{
+            paging: false,
+            search: true,
+            filtering: true,
+          }}
+          onRowClick={(event, row) => {
+            setRow(row);
+            setOpen(true);
+          }}
+        />
+      </InfiniteScroll>
+
       <Dialog onClose={handleClose} aria-labelledby="dialog-title" open={open}>
         <DialogTitle id="dialog-title" onClose={handleClose}>
           Modal
